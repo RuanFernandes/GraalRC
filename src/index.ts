f80846e218f55ff1e0a68d1f8700c14b0a556539
RuanFernandes <ruan_guimaraes2004@hotmail.com>
2025-03-28 04:14:28 -0300

    .

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { GraalRCConfig } from './types';
+
+async function main() {
+    const config: GraalRCConfig = {
+        host: 'listserver.graalonline.com',
+        port: 14922,
+        account: 'ruanf',
+        password: 'randrandbr',
+        nickname: 'Ruan',
+    };
+}
+
+main();
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface GraalRCConfig {
+    host: string;
+    port: number;
+    account: string;
+    password: string;
+    nickname: string;
+    saveDir?: string;
+}
