e8555e9b2d4cc6bb31c0672c5a83d83eec8fec82
RuanFernandes <ruan_guimaraes2004@hotmail.com>
2025-03-28 04:36:18 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import { ServerLister } from './serverLister';
 import { GraalRCConfig } from './types';
 
+import { config } from 'dotenv';
+
+const env = config({ path: './.env' });
+
 async function main() {
     const config: GraalRCConfig = {
         host: 'listserver.graalonline.com',
         port: 14922,
-        account: 'ruanf',
-        password: 'randrandbr',
+        account: env.parsed?.ACCOUNT || '',
+        password: env.parsed?.PASSWORD || '',
         nickname: 'Ruan',
     };
 
-    const serverlister = new ServerLister(config);
-    const servers = await serverlister.fetchServerList();
-    console.log('Servers:', servers);
+    console.log('Config:', config);
+
+    // const serverlister = new ServerLister(config);
+    // const servers = await serverlister.fetchServerList();
+    // console.log('Servers:', servers);
 }
 
 main();
